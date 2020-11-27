eea51134a7866e6b273158b22e6c23b9c2427eb6
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-11-27 17:15:47 +0100

    remove folders in package() not build()...

--- conanfile.py
+++ conanfile.py
@@ -53,13 +53,13 @@ class GlbindingConan(ConanFile):
             tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
-        tools.rmdir(os.path.join(self.package_folder, "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package(self):
         self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "cmake"))
+        tools.rmdir(os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.names["cmake_find_package"] = "glbinding"
