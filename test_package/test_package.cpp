e603138877d17eb2019f5a9fc93e3ecb86377cb7
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-11-27 18:22:03 +0100

    minor change

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -5,13 +5,14 @@ int main() {
     if (!glfwInit()) return 1;
 
     glfwDefaultWindowHints();
-    glfwWindowHint(GLFW_VISIBLE, false);
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 
     GLFWwindow *window = glfwCreateWindow(320, 240, "", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
 
     glbinding::initialize(glfwGetProcAddress, false);
